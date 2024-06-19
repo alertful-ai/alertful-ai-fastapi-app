b93017d0ad72f65e1214fed910bd85cb9e9085da
Joel Wang <joel@Joels-MacBook-Pro.local>
2024-06-19 15:16:41 -0700

    setting latestChange on page when adding new Change

--- scheduler.py
+++ scheduler.py
@@ -14,11 +14,18 @@ class Page(BaseModel):
     pageUrl: str
     query: str
     pageId: str
+    userId: int
+
+
+class Change(BaseModel):
+    changeId: str
+    pageId: str
 
 
 # load all pages
-page_response = supabase.table('Page').select("pageId", "pageUrl", "query").execute()
+page_response = supabase.table('Page').select('*').execute()
 pages = [Page(**page) for page in page_response.data]
+pages_by_page_id = object_dict = dict((page.pageId, page) for page in pages)
 
 # TODO: generate image_url from page_url
 image_url = ""
@@ -27,5 +34,19 @@ image_url = ""
 summary = ""
 
 # create Changes
-changes_to_insert = [{"summary": summary, "pageId": page.pageId, "imageUrl": image_url} for page in pages]
-response = supabase.table('Change').insert(changes_to_insert).execute()
+changes_to_insert = [{"summary": summary,
+                      "pageId": page.pageId,
+                      "imageUrl": image_url}
+                     for page in pages]
+changes_response = supabase.table('Change').insert(changes_to_insert).execute()
+
+# updates Pages with latest Change
+changes = [Change(**change) for change in changes_response.data]
+pages_to_update = [{"userId": pages_by_page_id[change.pageId].userId,
+                    "pageUrl": pages_by_page_id[change.pageId].pageUrl,
+                    "query": pages_by_page_id[change.pageId].query,
+                    "pageId": change.pageId,
+                    "latestChange": change.changeId}
+                   for change in changes]
+
+update_response = supabase.table('Page').upsert(pages_to_update).execute()
